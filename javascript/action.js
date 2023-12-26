9b87800700a7eda2bcd993479aed2b7e1a2bbbed
MENGLEANG22 <leangangel123@gmail.com>
2023-12-26 18:33:11 +0700

    Add button go to top

--- javascript/action.js
+++ javascript/action.js
@@ -11,3 +11,34 @@ if (document.body.classList.contains("dark-theme")){
     icon.src = "./images/moon2.png";
 }
 }
+
+let calcScrollVal = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressVal = document.getElementById("progress-val");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollVal = Math.round((pos * 100) / calcHeight);
+
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+
+    // Check if the event listener is already added before adding it
+    if (!scrollProgress._clickListenerAdded) {
+        scrollProgress.addEventListener("click", () => {
+            document.documentElement.scrollTop = 0;
+        });
+        // Mark that the event listener has been added
+        scrollProgress._clickListenerAdded = true;
+    }
+
+    scrollProgress.style.background = `conic-gradient(#1e90ff ${scrollVal}%, #d7d7d7 ${scrollVal}%)`;
+};
+
+// Initial call to calcScrollVal to set up the progress bar
+calcScrollVal();
+
+// Event listener for scroll events
+window.addEventListener("scroll", calcScrollVal);
